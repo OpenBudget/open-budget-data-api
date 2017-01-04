bc7df2f35404cce72ff8432c0551074a0f05edb1
Adam Kariv <adam.kariv@gmail.com>
2017-01-05 00:57:24 +0200

    stashing

--- open_budget_data_api/config.py
+++ open_budget_data_api/config.py
@@ -1,6 +1,14 @@
+import os
+
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
-database = PostgresqlExtDatabase(host='data.obudget.org', port=5432,
-                                 user='redash_reader', password='kaedusha',
-                                 database='obudget',
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_USER = os.environ.get('DB_USER')
+DB_PWD = os.environ.get('DB_PWD')
+DB_DB = os.environ.get('DB_DB')
+
+database = PostgresqlExtDatabase(host=DB_HOST, port=DB_PORT,
+                                 user=DB_USER, password=DB_PWD,
+                                 database=DB_DB,
                                  threadlocals=True, register_hstore=False)
