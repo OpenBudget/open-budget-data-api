6a1d5bd5c9ca083183f4af7ceb45f291643b1a48
Adam Kariv <adam.kariv@gmail.com>
2017-10-03 17:32:04 +0300

    Jsonize results

--- open_budget_data_api/db.py
+++ open_budget_data_api/db.py
@@ -1,6 +1,9 @@
 import os
 
 import logging
+from datetime import date
+
+from decimal import Decimal
 from sqlalchemy import create_engine
 from itertools import islice
 
@@ -11,6 +14,17 @@ engine = create_engine(os.environ['DATABASE_URL'], pool_size=20, max_overflow=0)
 log.info('Attempting connection...')
 engine.connect()
 log.info('Connection Okay')
+
+def jsonable(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    if isinstance(obj, date):
+        return obj.isoformat()
+    if isinstance(obj, list):
+        return [jsonable(x) for x in obj]
+    if isinstance(obj, dict):
+        return dict((k, jsonable(v)) for k, v in obj.items())
+    return obj
 
 
 def query_db(query_str):
@@ -24,6 +38,7 @@ def query_db(query_str):
             log.info('executing %r', query)
             result = connection.execute(query)
             rows = list(map(dict, islice(iter(result), 0, 1000)))
+            rows = [jsonable(row) for row in rows]
             log.info('rowcount %r', len(rows))
     except:
         log.exception('EXC')
