48efca8d12cc096767fa07a3bf92fcfab4e67e05
Adam Kariv <adam.kariv@gmail.com>
2018-02-05 22:15:58 +0200

    Allow download xslx

--- open_budget_data_api/main.py
+++ open_budget_data_api/main.py
@@ -1,9 +1,11 @@
 import csv
 import logging.config
+import tempfile
+
+import xlsxwriter
 
 from io import StringIO
-from flask import Flask, request
-from flask import Response
+from flask import Flask, request, abort, Response, send_file
 from flask_jsonpify import jsonpify
 from flask_cors import CORS
 
@@ -30,27 +32,51 @@ def query():
 @app.route('/api/download')
 def download():
     results = query_db_streaming(request.values.get('query'))
-    headers = {'Content-Type': 'text/csv',
-               'Content-Disposition': 'attachment; filename=budgetkey.csv'}
+    format = request.values.get('format', 'csv')
+    if format not in ('csv', 'xlsx'):
+        abort(400)
+    mime = {
+        'csv': 'text/csv',
+        'xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    }[format]
 
-    def generate():
-        buffer = StringIO()
-        try:
-            headers = next(results)
-        except:
-            return
-        writer = csv.DictWriter(buffer, headers)
-        writer.writeheader()
-        for row in results:
-            writer.writerow(row)
-            pos = buffer.tell()
-            buffer.seek(0)
-            ret = buffer.read(pos)
-            buffer.seek(0)
-            yield ret
+    if format=='csv':
+        def generate():
+            buffer = StringIO()
+            try:
+                headers = next(results)
+            except:
+                return
+            writer = csv.DictWriter(buffer, headers)
+            writer.writeheader()
+            for row in results:
+                writer.writerow(row)
+                pos = buffer.tell()
+                buffer.seek(0)
+                ret = buffer.read(pos)
+                buffer.seek(0)
+                yield ret
 
-    return Response(generate(),
-                    content_type='text/csv', headers=headers)
+        headers = {'Content-Type': mime,
+                    'Content-Disposition': 'attachment; filename=budgetkey.csv'}
+        return Response(generate(),
+                        content_type='text/csv', headers=headers)
+    if format=='xlsx':
+        with tempfile.NamedTemporaryFile(mode='w+b', suffix='.xslx') as out:
+            try:
+                workbook = xlsxwriter.Workbook(out.name)
+                worksheet = workbook.add_worksheet()
+                headers = next(results)
+                for j, h in enumerate(headers):
+                    worksheet.write(0, j, h)
+                for i, row in enumerate(results):
+                    for j, h in enumerate(headers):
+                        v = row.get(h)
+                        if v is not None:
+                            worksheet.write(i+1, j, v)
+            finally:
+                workbook.close()
+            return send_file(out.name, mimetype=mime, as_attachment=True, attachment_filename='budgetkey.xlsx')
 
 initialize_app(app)
 logging.error('Initialized App')
