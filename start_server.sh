8c505400ed80afc9ccb5bf5b5c21e166d3012aa6
Adam Kariv <adam.kariv@gmail.com>
2019-10-06 13:18:02 +0300

    Fix dev server port

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-gunicorn  -t 120 -b 0.0.0.0:8888 --reload --log-level debug open_budget_data_api.main:app 
+gunicorn  -t 120 -b 0.0.0.0:8000 --reload --log-level debug open_budget_data_api.main:app 
